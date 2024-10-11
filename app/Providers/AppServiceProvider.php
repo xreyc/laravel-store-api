592faf8ec141359a273f4f590520d5ec72d202f8
Reyco Seguma <rseguma@lula.life>
2024-10-11 22:01:28 +0800

    updatE

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,6 +21,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        // Load passport keys
         Passport::loadKeysFrom(__DIR__.'/../secrets/oauth');
+        // Token lifetimes
+        Passport::tokensExpireIn(now()->addDays(15));
+        Passport::refreshTokensExpireIn(now()->addDays(30));
+        Passport::personalAccessTokensExpireIn(now()->addMonths(6));
     }
 }
